238f53dd4da707b147476fa2bdd1ecea12f45a2f
Francesco-Rianna <maradonino98@hotmail.it>
2024-03-11 15:24:11 +0100

    feat : added third snack

---
+++ third-snack/js/script.js
@@ -0,0 +1,21 @@
+// - Crea un array vuoto.
+//   Chiedi per 6 volte all’utente di inserire un numero,
+//   se è dispari inseriscilo nell’array.
+
+// creo array vuoto
+const numbers = []
+// inizio il ciclo chiedendo 6 volte il numero all'utente
+
+for (i = 0; i < 6; i++) {
+    const userNumbers = parseInt(prompt('dimmi un numero'))
+
+    if (userNumbers % 2 === 0) {
+        numbers.push()
+    } else {
+        numbers.push(userNumbers)
+    }
+
+
+}
+
+console.log(numbers)
